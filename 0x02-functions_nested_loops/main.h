2ce35a8391eebdd5459f11b5ed4e3c4036a85331
Gabriel <gabrielbankole07@gmail.com>
2022-10-18 16:18:20 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,4 @@
-#include <unistd.h> 
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
 
-/** 
-* _putchar - writes the character c to stdout 
-* @c: The character to print 
-* Return: On success 1. 
-* On error, -1 is returned, and error is set appropriately.
-*/ 
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
