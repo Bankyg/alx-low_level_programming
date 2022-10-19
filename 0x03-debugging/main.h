6a3e1023332d22d8ad13275cca29712d55fae020
Gabriel <gabrielbankole07@gmail.com>
2022-10-19 16:51:15 -0700

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,32 +6,3 @@
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
-#include "main.h"
-
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-
-int largest_number(int a, int b, int c)
-{
-    int largest;
-
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
