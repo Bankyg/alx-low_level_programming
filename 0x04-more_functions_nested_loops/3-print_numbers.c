eb2c609ed11c030a9239a6a862645382ca23f01c
Gabriel <gabrielbankole07@gmail.com>
2022-10-21 16:32:38 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
 * print_numbers - print 0123456789\n
-* 
 * Return: void
 */
 void print_numbers(void)
 {
-char c;                                                   for (c = '0'; c <= '9'; c++)
+char c;
+for (c = '0'; c <= '9'; c++)
 {
 _putchar(c);
 }
