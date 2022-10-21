dd7a779a3e9f8ff2b2edaad874c1635c4bf76db4
Gabriel <gabrielbankole07@gmail.com>
2022-10-20 21:06:27 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiply 2 int
+ * @a: furst num
+ * @b: second num
+ * Return: result of  multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
