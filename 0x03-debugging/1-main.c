070607fae0cf1aea8e4c8b83e2980480ab310d8c
Gabriel <gabrielbankole07@gmail.com>
2022-10-21 18:45:39 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - cause an infinite loop
 * Return: 0
 */
 
@@ -15,7 +15,7 @@ int main(void)
 	/**
 	*while (i < 10)
 	*{
-	*	 putchar(i);
+	* putchar(i);
 	*}
 	*/ 
 	printf("Infinite loop avoided! \\o/\n");
