aafae3db97d42698b4d5f368b76335c3bbd153bf
Gabriel <gabrielbankole07@gmail.com>
2022-10-28 14:52:03 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef main.h
+#define main.h
 
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
