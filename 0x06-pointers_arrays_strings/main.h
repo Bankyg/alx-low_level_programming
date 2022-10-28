02bb28872d152baa4a64870e2fd33a7ea060fe7a
Gabriel <gabrielbankole07@gmail.com>
2022-10-28 14:58:21 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef main.h
 #define main.h
-char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
