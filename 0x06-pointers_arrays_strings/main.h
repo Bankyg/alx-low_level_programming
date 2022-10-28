4f69d3916e1d8be766cf2287fae55bccd2bac8a1
Gabriel <gabrielbankole07@gmail.com>
2022-10-28 14:55:44 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
 #ifndef main.h
 #define main.h
-
-char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
