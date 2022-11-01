4a1b5359a43fad01d76204686cafa89e2b7d873a
Gabriel <gabrielbankole07@gmail.com>
2022-10-31 18:46:06 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,7 +9,7 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	
+
 	for (i = 0; s[i] >= '\0'  ; i++)
 	{
 		if (s[i] == c)
@@ -17,6 +17,6 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
-	
+
 	return ('\0');
 }
